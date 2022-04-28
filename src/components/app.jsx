68fd4fe25a1aea27c78f0349bc94b40e4134bc36
Clement Azalbert <clementazalbert04@gmail.com>
2022-04-28 14:24:51 +0200

    created all components + giphy api

---
+++ src/components/app.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import giphyApi from 'giphy-api';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "3s0ddui7kadGg"
+    };
+  }
+
+  search = (query) => {
+    // Call the Giphy API with API key created on Giphy Developper
+    giphyApi('z45leHbVfCfS0rA5jqY5PLdHOhQaBFNO').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  handleClick = () => {
+    this.setState({
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/hello.jsx
@@ -0,0 +1,16 @@
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
+
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  constructor(props) {
+    super(props)
+  }
+}
